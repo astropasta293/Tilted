0f06a97ab81a3803101962634980e07e29fee563
AstroPasta <astropasta23433@outlook.com>
2025-10-20 00:53:56 -0400

    Add Collision

--- ball_rigid_body_2d.gd
+++ ball_rigid_body_2d.gd
@@ -2,13 +2,14 @@ extends RigidBody2D
 const speed = 100
 
 func _physics_process(delta: float):
-		
 	if Input.is_key_pressed(KEY_W):
 		apply_central_force(Vector2.UP * speed)
-	elif Input.is_key_pressed(KEY_S):
+	if Input.is_key_pressed(KEY_S):
 		apply_central_force(Vector2.DOWN * speed)
-	elif Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_D):
 		apply_central_force(Vector2.RIGHT * speed)
-	elif Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_A):
 		apply_central_force(Vector2.LEFT * speed)
+	if !Input.is_key_pressed(KEY_W) && !Input.is_key_pressed(KEY_S) && !Input.is_key_pressed(KEY_D) && !Input.is_key_pressed(KEY_A):
+				linear_velocity = linear_velocity.move_toward(Vector2.ZERO, delta * speed)
 	
